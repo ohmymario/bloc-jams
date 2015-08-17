071be6d7957bbc088056484b1d4dd12e6626bcf9
Mario <mariob360@gmail.com>
2015-08-17 15:21:23 -0700

    fixed for assignment

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,6 @@
 
 
+/*
 function forEach(x, callback) {
     for (var i = 0; i < x.length; i++) {
         callback([i, x[i]]);
@@ -14,7 +15,23 @@ function myCallBack(y) {
 
 
 forEach(["mario", "person1"], myCallBack);
+*/
 
+
+var forEach = function (x, callback) {
+    for (var i = 0; i < x.length; i++) {
+        callback(i);
+    }
+};
+
+function revealPoint(y) {
+    pointsArray[y].style.opacity = 1;
+    pointsArray[y].style.transform = "scaleX(1) translateY(0)";
+    pointsArray[y].style.msTransform = "scaleX(1) translateY(0)";
+    pointsArray[y].style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
+forEach(pointsArray, revealPoint);
 
 
 
