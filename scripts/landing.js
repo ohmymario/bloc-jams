491a083760b56e8c234a77a477df9f7d790fbbba
Mario <mariob360@gmail.com>
2015-08-19 13:22:47 -0700

    fixed the assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,6 @@
 var pointsArray = document.getElementsByClassName('point');
 
 
-var animatePoints = function (x, callback) {
-    for (var i = 0; i < x.length; i++) {
-        callback(i);
-    }
-};
-
 function revealPoint(y) {
     pointsArray[y].style.opacity = 1;
     pointsArray[y].style.transform = "scaleX(1) translateY(0)";
@@ -17,13 +11,13 @@ function revealPoint(y) {
 window.onload = function () {
 
     if (window.innerHeight > 950) {
-        animatePoints(pointsArray, revealPoint);
+        forEach(pointsArray, revealPoint);
     }
 
     window.addEventListener('scroll', function (event) {
 
         if (pointsArray[0].getBoundingClientRect().top <= 500) {
-            animatePoints(pointsArray, revealPoint);
+            forEach(pointsArray, revealPoint);
         }
 
     });
