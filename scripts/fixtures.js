c2c615a70e910042fd8fff1e8a1b5a4d14f79bff
Mario <mariob360@gmail.com>
2015-10-01 14:09:24 -0700

    checkpoint before merge

---
+++ scripts/fixtures.js
@@ -0,0 +1,46 @@
+var albumPicasso = {
+     name: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Blue', length: '4:26' },
+         { name: 'Green', length: '3:14' },
+         { name: 'Red', length: '5:01' },
+         { name: 'Pink', length: '3:21'},
+         { name: 'Magenta', length: '2:15'}
+     ]
+ };
+ 
+ // Another Example Album
+ 
+var albumMarconi = {
+     name: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { name: 'Hello, Operator?', length: '1:01' },
+         { name: 'Ring, ring, ring', length: '5:01' },
+         { name: 'Fits in your pocket', length: '3:21'},
+         { name: 'Can you hear me now?', length: '3:14' },
+         { name: 'Wrong phone number', length: '2:15'}
+     ]
+ };
+
+var albumMyAlbum = {
+     name: 'Listen to My Album',
+     artist: 'Unknown',
+     label: 'Long Song',
+     year: '2020',
+     albumArtUrl: 'assets/images/album_covers/21.png',
+     songs: [
+         { name: 'First song', length: '10:01' },
+         { name: '2nd Song', length: '5:51' },
+         { name: 'Song after 2nd', length: '46:21'},
+         { name: 'Close to the end', length: '3:14' },
+         { name: 'End of album', length: '87:01'}
+     ]
+ };
